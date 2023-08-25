0e2bee83f827f7b823b2cc48b4d35001eb0b6bd6
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-25 16:21:16 +0200

    used Widget component and created css for that

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import "./home.scss";
 import { Sidebar } from "../../components/sidebar/Sidebar";
 import { Navbar } from "../../components/navbar/Navbar";
+import { Widget } from "../../components/widget/Widget";
 
 export const Home = () => {
   return (
     <div className="home">
       <Sidebar />
       <div className="home-container">
-        <Navbar/>
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earnings" />
+          <Widget type="balance" />
+        </div>
       </div>
     </div>
   );
