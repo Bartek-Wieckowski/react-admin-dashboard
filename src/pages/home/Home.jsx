6dd961a7db04e0a47f51e04ef81f2d9b925b5680
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-25 15:38:49 +0200

    used Navbar component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./home.scss";
 import { Sidebar } from "../../components/sidebar/Sidebar";
+import { Navbar } from "../../components/navbar/Navbar";
 
 export const Home = () => {
   return (
     <div className="home">
       <Sidebar />
-      <div className="home-container">container</div>
+      <div className="home-container">
+        <Navbar/>
+      </div>
     </div>
   );
 };
