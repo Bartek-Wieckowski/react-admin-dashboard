28481e9649f369ff4c65ab58f13b1b62709a9e21
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-26 06:19:25 +0200

    used Datatable component and created css

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+import "./list.scss";
+import { Sidebar } from "../../components/sidebar/Sidebar";
+import { Navbar } from "../../components/navbar/Navbar";
+import DataTable from "../../components/datatable/Datatable";
+
 
 export const List = () => {
   return (
-    <div>List</div>
-  )
-}
+    <div className="list">
+      <Sidebar />
+      <div className="list-container">
+        <Navbar />
+        <DataTable/>
+      </div>
+    </div>
+  );
+};
