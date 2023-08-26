ec7acc93b62a00e6277d98d1020e04183556873b
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-26 05:38:21 +0200

    used Table component and added basic css for them

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import { Navbar } from "../../components/navbar/Navbar";
 import { Widget } from "../../components/widget/Widget";
 import { Featured } from "../../components/featured/Featured";
 import { Chart } from "../../components/chart/Chart";
+import  Table  from "../../components/table/Table";
 
 export const Home = () => {
   return (
@@ -20,7 +21,11 @@ export const Home = () => {
         </div>
         <div className="charts">
           <Featured />
-          <Chart title="Last 6 Months (Revenue)" aspect={2 / 1}/>
+          <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
+        </div>
+        <div className="list-container">
+          <div className="list-title">Latest Transactions</div>
+          <Table />
         </div>
       </div>
     </div>
