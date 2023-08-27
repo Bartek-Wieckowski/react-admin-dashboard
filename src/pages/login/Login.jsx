506aa3fe6f5b71ed1c81f0f5de9c239617cf28d7
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-27 07:05:41 +0200

    used dispatch in success login and set payload

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,13 +3,15 @@ import "./login.scss";
 import { auth } from "../../firebaseconfig";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../../context/AuthContext";
 
 export const Login = () => {
   const [error, setError] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  
+
   const navigate = useNavigate();
+  const { dispatch } = useAuthContext();
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -17,8 +19,8 @@ export const Login = () => {
     try {
       const userCredential = await signInWithEmailAndPassword(auth, email, password);
       const user = userCredential.user;
-      
-      console.log(user);
+
+      dispatch({ type: "login", payload: user });
 
       setError(false);
       navigate("/");
