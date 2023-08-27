ffb66104bccb927b715265b2a786555bc931e5ed
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-27 19:45:12 +0200

    fetched data with firebase func and used useEffect

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,33 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../datatableusers";
 import { Link } from "react-router-dom";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebaseconfig";
 
 export default function DataTable() {
-  const [data, setData] = useState(userRows);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let list = [];
+      try {
+        const querySnapshot = await getDocs(collection(db, "users"));
+        querySnapshot.forEach((doc) => {
+          list.push({ id: doc.id, ...doc.data() });
+          setData(list);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
 
   function handleDelete(id) {
     setData(data.filter((item) => item.id !== id));
   }
 
-  const columnsWithAction = [...userColumns, {
-    field: "action",
-    headerName: "Action",
-    width: 200,
-    renderCell: (params) => {
-      return (
-        <div className="cell-action">
-          <Link to="/users/test" style={{ textDecoration: "none" }}>
-            <div className="view-button">View</div>
-          </Link>
-          <div className="delete-button" onClick={() => handleDelete(params.id)}>
-            Delete
+  const columnsWithAction = [
+    ...userColumns,
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="cell-action">
+            <Link to="/users/test" style={{ textDecoration: "none" }}>
+              <div className="view-button">View</div>
+            </Link>
+            <div className="delete-button" onClick={() => handleDelete(params.id)}>
+              Delete
+            </div>
           </div>
-        </div>
-      );
+        );
+      },
     },
-  }];
+  ];
 
   return (
     <div className="datatable">
